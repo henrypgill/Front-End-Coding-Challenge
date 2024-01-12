3e14b639e36e0d17e584d5ab8745f8369f461417
henrypgill <hpgill312@gmail.com>
2024-01-12 14:10:11 +0000

    added comments to store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,18 +2,18 @@ import { Action, ThunkAction, configureStore } from "@reduxjs/toolkit";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import opviaTableReducer from "./opviaTableSlice"
 
-
+// export a type that represents the data structure of the store
 export type RootState = ReturnType<typeof store.getState>;
 type AppDispatch = typeof store.dispatch;
 
-
+// declare and export the redux store
 const store = configureStore({
   reducer: {
     opviaTable: opviaTableReducer,
   },
 });
-
 export default store
 
+//export the store's dispatch and selector functions for ease of use
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
