5b36e73183229507692d3ff082ca379f35b30884
henrypgill <hpgill312@gmail.com>
2024-01-12 14:18:35 +0000

    added button to add a column to the table

--- src/table/TableButtons.tsx
+++ src/table/TableButtons.tsx
@@ -3,27 +3,35 @@ import * as React from 'react';
 import { Cell, Column, Table2 } from '@blueprintjs/table';
 import { dummyTableData } from '../data/dummyData';
 import { ButtonGroup, Button, AnchorButton } from '@blueprintjs/core';
-
+import { useAppDispatch } from '../redux/store';
+import { opviaTableActions } from '../redux/opviaTableSlice';
 
 const TableButtons: React.FC = () => {
 
-    const buttons = () => {
-        return (
-            <>
-            <Button icon="database">Queries</Button>
-            <Button icon="function">Functions</Button>
-            <AnchorButton rightIcon="caret-down">Options</AnchorButton>
-            </>
-            )
-    }
+  const dispatch = useAppDispatch()
+  const tableActions = opviaTableActions
 
+  const addColumnHandler = () => {
+    dispatch(tableActions.addColumn({
+        columnName: 'Name', columnType: 'data', columnId: 'var_col_3'
+      }))
+  }
+
+  const buttons = () => {
     return (
-        <div>
-            <ButtonGroup minimal={true} children={buttons()}>
-            </ButtonGroup>
-        </div>
-    )
+      <>
+        <Button icon="add-column-right" rightIcon="caret-down" onClick={addColumnHandler}>
+          Add Column
+        </Button>
+      </>
+    );
+  };
 
+  return (
+    <div>
+      <ButtonGroup minimal={true} children={buttons()}></ButtonGroup>
+    </div>
+  );
 };
 
 export default TableButtons;
