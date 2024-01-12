77ef93511c7ae68c1d25df4768d28c66fe2d2194
henrypgill <hpgill312@gmail.com>
2024-01-12 14:10:27 +0000

    add the store as a provider around the app

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,15 @@ import { HotkeysProvider } from '@blueprintjs/core';
 import App from './App';
 import 'normalize.css';
 import './index.css';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <HotkeysProvider>
+    <Provider store={store}>
     <App />
+    </Provider>
   </HotkeysProvider>,
 );
