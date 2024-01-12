3706fdccfd7ae9264fad7841281107d99f5ba7a5
henrypgill <hpgill312@gmail.com>
2024-01-12 14:46:13 +0000

    moved TableButtons into header directory

---
+++ src/components/header/TableButtons.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+
+import { Cell, Column, Table2 } from '@blueprintjs/table';
+import { dummyTableData } from '../../data/dummyData';
+import { ButtonGroup, Button, AnchorButton } from '@blueprintjs/core';
+import { useAppDispatch } from '../../redux/store';
+import { opviaTableActions } from '../../redux/opviaTableSlice';
+
+const TableButtons: React.FC = () => {
+  const dispatch = useAppDispatch();
+  const tableActions = opviaTableActions;
+
+  const addColumnHandler = () => {
+    dispatch(
+      tableActions.addColumn({
+        columnName: 'Name',
+        columnType: 'data',
+        columnId: 'var_col_3',
+      }),
+    );
+  };
+
+  const buttons = () => {
+    return (
+      <>
+        <Button
+          icon="add-column-right"
+          rightIcon="caret-down"
+          onClick={addColumnHandler}
+        >
+          Add Column
+        </Button>
+      </>
+    );
+  };
+
+  return (
+    <div>
+      <ButtonGroup minimal={true} children={buttons()}></ButtonGroup>
+    </div>
+  );
+};
+
+export default TableButtons;
