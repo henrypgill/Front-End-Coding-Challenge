6bcffd7709386c75a01b6ecc7e8085ad307dafdf
henrypgill <hpgill312@gmail.com>
2024-01-12 14:12:21 +0000

    Update tableGroup to use state from redux store

--- src/table/TableGroup.tsx
+++ src/table/TableGroup.tsx
@@ -2,19 +2,12 @@ import * as React from 'react';
 
 import OpviaTable from "./OpviaTable"
 import TableButtons from "./TableButtons"
+import { useAppSelector } from '../redux/store';
 
-const defaultColumns = [
-    { columnName: 'Time', columnType: 'time', columnId: 'time_col' },
-    {
-      columnName: 'Cell Density (Cell Count/Litre)',
-      columnType: 'data',
-      columnId: 'var_col_1',
-    },
-    { columnName: 'Volume (Litres)', columnType: 'data', columnId: 'var_col_2' },
-  ];
+
 const TableGroup = () => {
 
-    const [columns, setColumns] = React.useState(defaultColumns)
+  const {data, columns} = useAppSelector((state) => state.opviaTable)
 
     return (
         <>
