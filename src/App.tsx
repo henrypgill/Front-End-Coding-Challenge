cf7320878d9ca645ee430b84c491e768621e9e0c
henrypgill <hpgill312@gmail.com>
2024-01-12 14:44:37 +0000

    removed text from template and added header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,15 @@
 import { Classes } from '@blueprintjs/core';
 import React from 'react';
 import './App.css';
-import OpviaTable from './table/OpviaTable';
-import TableGroup from './table/TableGroup';
+import OpviaTable from './components/table/OpviaTable';
+import TableGroup from './components/table/TableGroup';
+import OpviaNavbar from './components/header/OpviaNavbar';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <div style={{ height: 50 }}></div>
-      <div style={{ paddingBottom: 10 }} className={Classes.TEXT_LARGE}>
-        This is an empty template with the blueprintjs table installed and
-        working with some dummy data. You can use this as your starting off
-        point.
-      </div>
-
       <div>
-        Please carefully read the instructions in the readme on github{' '}
-        <a href="https://github.com/opvia/column-cals-interview">here.</a>
+        <OpviaNavbar />
       </div>
       <div style={{ padding: 75 }}>
         <TableGroup />
