bc1c5d64bc326e6160c65fede4a0be042e50461a
henrypgill <hpgill312@gmail.com>
2024-01-13 19:16:24 +0000

    Other approaches I tried

---
+++ src/components/header/CreateFxColumnDialog.tsx
@@ -0,0 +1,33 @@
+/*
+Note: explored using a popup dialog to enter the details for the equation of a calculation
+column, however I ran into some issues getting it to work how I wanted it to and I felt it
+ruined the flow of the app, thus I opted to use a menu in the column header instead
+*/
+
+// import { Button, Dialog, DialogBody, DialogFooter } from '@blueprintjs/core';
+
+// interface CreateFxColumnDialogProps {
+//     isOpen: boolean;
+//     handleDialogClose(): void;
+// }
+
+// const CreateFxColumnDialog = ({
+//     isOpen,
+//     handleDialogClose,
+// }: CreateFxColumnDialogProps) => {
+//     return (
+//         <Dialog isOpen={isOpen} title="Create New Calculation Column">
+//             <DialogBody>
+//                 <p>
+//                     <strong>Add Column</strong>
+//                 </p>
+//                 <p>Create a new calculation column</p>
+//             </DialogBody>
+//             <DialogFooter>
+//                 <Button onClick={handleDialogClose}>close</Button>
+//             </DialogFooter>
+//         </Dialog>
+//     );
+// };
+
+// export default CreateFxColumnDialog;
