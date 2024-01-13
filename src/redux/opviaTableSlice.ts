55984472bbc5db1b32d3b023ce62e66063d591fe
henrypgill <hpgill312@gmail.com>
2024-01-13 14:39:58 +0000

    updated types of the table slice to utilise new data structure and mapData function

--- src/redux/opviaTableSlice.ts
+++ src/redux/opviaTableSlice.ts
@@ -1,15 +1,22 @@
 import { PayloadAction, createSlice } from '@reduxjs/toolkit';
 import { RootState } from './store';
 import { dummyTableData } from '../data/dummyData';
+import { mapData } from '../data/mapData';
 
 // define the type for the data containing object that is passed to the table
-export type TableData = { [key: string]: string | number };
+export type TableData = {
+    [key: string]: { [key: string]: string | number };
+};
 
-// define an interface that represents a column to be rendered in the table
+// define types and an interface that represents a column to be rendered in the table
+export type ColumnType = 'time' | 'number' | 'string' | 'function';
+export type ColumnFunction = void;
+
 export interface OpviaTableColumn {
     columnName: string;
-    columnType: string;
+    columnType: ColumnType;
     columnId: string;
+    columnFunction?: ColumnFunction;
 }
 
 // define an interface for the state of the OpviaTable
@@ -19,35 +26,35 @@ export interface OpviaTableState {
 }
 
 // declare the initial state of the columns for the table
-const defaultColumns = [
+const defaultColumns: OpviaTableColumn[] = [
     { columnName: 'Time', columnType: 'time', columnId: 'time_col' },
     {
         columnName: 'Cell Density (Cell Count/Litre)',
-        columnType: 'data',
+        columnType: 'number',
         columnId: 'var_col_1',
     },
     {
         columnName: 'Volume (Litres)',
-        columnType: 'data',
+        columnType: 'number',
         columnId: 'var_col_2',
     },
 ];
 
 // declare the initial state for the slice
-const initialState = {
-    data: dummyTableData,
+const initialState: OpviaTableState = {
+    data: mapData(dummyTableData),
     columns: defaultColumns,
 };
 
 // create the redux slice
 export const opviaTableSlice = createSlice({
-  name: 'opviaTable',
-  initialState,
-  reducers: {
-    addFxColumn: (state, action: PayloadAction<OpviaTableColumn>) => {
-      state.columns.push(action.payload);
+    name: 'opviaTable',
+    initialState,
+    reducers: {
+        addFxColumn: (state, action: PayloadAction<OpviaTableColumn>) => {
+            state.columns.push(action.payload);
+        },
     },
-  },
 });
 
 // export the slice's reducer
