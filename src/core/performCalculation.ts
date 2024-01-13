9a3e19e1e2defe046e201e0f4e24eef51920ddf9
henrypgill <hpgill312@gmail.com>
2024-01-13 19:15:10 +0000

    created simple function to perform cell calculations

---
+++ src/core/performCalculation.ts
@@ -0,0 +1,15 @@
+import { ColumnFunctionOperator } from "../redux/opviaTableSlice";
+
+
+export function performCalculation(num1: number, num2: number, operator: ColumnFunctionOperator): number {
+    switch (operator) {
+        case "*":
+            return num1 * num2;
+        case "/":
+            return num1 / num2;
+        case "+":
+            return num1 + num2;
+        case "-":
+            return num1 - num2;
+    }
+}
