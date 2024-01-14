dbbf74865a70a0cb8030170263313ba7ff2338e5
henrypgill <hpgill312@gmail.com>
2024-01-14 14:48:45 +0000

    created function for getting icon of aggregate card

---
+++ src/components/analysis/getAggregateIcon.ts
@@ -0,0 +1,14 @@
+import { AggregateType } from "../../redux/analysisSlice";
+
+
+
+const getAggregateIcon = (aggregateType: AggregateType) => {
+    switch (aggregateType) {
+        case "maximum":
+            return "add";
+        case "minimum": 
+            return "minus"
+    }
+}
+
+export default getAggregateIcon
