722ddce77e8c8cfa9fd18aeaf873bb2bfcb746cb
henrypgill <hpgill312@gmail.com>
2024-01-14 21:50:26 +0000

    added reducer for updating aggregate to analysisSlice along with accompanying types

--- src/redux/analysisSlice.ts
+++ src/redux/analysisSlice.ts
@@ -1,8 +1,7 @@
 import { PayloadAction, createSlice } from '@reduxjs/toolkit';
 
-
 // define an interface for the state of the OpviaAnalysis Slice
-export type AggregateType = "maximum" | "minimum"
+export type AggregateType = 'maximum' | 'minimum';
 export interface Aggregate {
     type: AggregateType;
     columnIndex: number;
@@ -13,21 +12,47 @@ export interface OpviaAnalysisState {
     aggregates: Aggregate[];
 }
 
-
 // declare the initial state for the slice
 const initialState: OpviaAnalysisState = {
     aggregates: [
     ],
 };
 
+
+export interface AggregateColumnUpdate {
+    key: 'columnIndex';
+    value: number;
+}
+
+export interface AggregateTypeUpdate {
+    key: 'type';
+    value: AggregateType;
+}
+
+export type AggregateUpdate = AggregateColumnUpdate | AggregateTypeUpdate;
+
 // create the redux slice
 export const analysisSlice = createSlice({
     name: 'analysis',
     initialState,
     reducers: {
-        addAggregate: (state, {payload}: PayloadAction<Omit<Aggregate, "aggregateId">> ) => {
-            state.aggregates.push({...payload, aggregateId: `agg_max_${state.aggregates.length}`})
-        }
+        addAggregate: (
+            state,
+            { payload }: PayloadAction<Omit<Aggregate, 'aggregateId'>>,
+        ) => {
+            state.aggregates.push({
+                ...payload,
+                aggregateId: `agg_max_${state.aggregates.length}`,
+            });
+        },
+        updateAggregate: (
+            state, {payload}: PayloadAction<Aggregate>,
+        ) => {
+            return {
+                ...state,
+                aggregates: state.aggregates.map(agg => agg.aggregateId === payload.aggregateId ? payload : agg)
+            }
+        },
     },
 });
 
