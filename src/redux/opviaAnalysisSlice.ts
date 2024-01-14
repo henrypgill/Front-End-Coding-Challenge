34dd0249d8bec060713a33191221fc4ab6167d8d
henrypgill <hpgill312@gmail.com>
2024-01-14 13:47:43 +0000

    create skeleton for the analysis slice

---
+++ src/redux/opviaAnalysisSlice.ts
@@ -0,0 +1,31 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+
+// define an interface for the state of the OpviaAnalysis Slice
+type AggregateType = "maximum" | "minimum"
+interface Aggregate {
+    type: AggregateType;
+    columnIndex: number;
+}
+export interface OpviaAnalysisState {
+    aggregates: Aggregate[];
+}
+
+
+// declare the initial state for the slice
+const initialState: OpviaAnalysisState = {
+    aggregates: [],
+};
+
+// create the redux slice
+export const opviaAnalysisSlice = createSlice({
+    name: 'analysis',
+    initialState,
+    reducers: {
+    },
+});
+
+// export the slice's reducer
+export default opviaAnalysisSlice.reducer;
+//export the slices actions for ease of use in components
+export const opviaAnalysisActions = opviaAnalysisSlice.actions;
