d5053e2482b8d2949196825a9794916ab592fabc
henrypgill <hpgill312@gmail.com>
2024-01-16 19:26:09 +0000

    redesigned analysis section to seperate aggregates into sections by column

--- src/analysis/opviaAnalysis.tsx
+++ src/analysis/opviaAnalysis.tsx
@@ -1,31 +1,82 @@
-import { Card, Section } from '@blueprintjs/core';
+import { Card, EntityTitle, H4, Section } from '@blueprintjs/core';
 import { useAppSelector } from '../redux/store';
+import { Aggregate } from '../types/analysisTypes';
 import AggregateCard from './AggregateCard';
-import { OpviaTableColumn } from '../types/tableTypes';
 
-const AnalysisColumnSection: React.FC<OpviaTableColumn> = (column) => {
-    return <Card></Card>;
+interface AnalysisColumnSectionProps {
+    columnName: string;
+    aggregates: Aggregate[];
+}
+
+const AnalysisColumnSection: React.FC<AnalysisColumnSectionProps> = ({
+    columnName,
+    aggregates,
+}) => {
+    const aggregateCards = () =>
+        aggregates.map((agg) => (
+            <AggregateCard key={agg.aggregateId} aggregate={agg} />
+        ));
+    return (
+        <Card
+            style={{
+                display: 'flex',
+                flexDirection: 'column',
+                rowGap: 8,
+            }}
+        >
+            <EntityTitle title={columnName} heading={H4} />
+            {aggregateCards()}
+        </Card>
+    );
 };
 
 const Analysis = () => {
-    const { analysis } = useAppSelector((state) => state.table);
+    const {
+        analysis: { aggregates },
+        columns,
+    } = useAppSelector((state) => state.table);
 
-    if (!analysis.aggregates[0]) return <></>;
-    const aggregateCards = analysis.aggregates.map((agg) => (
+    if (!aggregates[0]) return <></>;
+    const aggregateCards = aggregates.map((agg) => (
         <AggregateCard aggregate={agg} key={agg.aggregateId} />
     ));
+
+    const analysisColumnSections = columns.map((col) => {
+        const colAggregates = aggregates.filter(
+            (agg) => agg.columnIndex === col.columnIndex,
+        );
+        if (!colAggregates[0]) return undefined;
+        return (
+            <AnalysisColumnSection
+                columnName={col.columnName}
+                aggregates={colAggregates}
+                key={col.columnId}
+            />
+        );
+    });
 
     return (
         <Section
             title={'Analysis'}
+            icon="chart"
             style={{
+                padding: 16,
                 display: 'flex',
                 flexDirection: 'row',
                 flexWrap: 'wrap',
-                columnGap: 16,
             }}
         >
-            {aggregateCards}
+            <div
+                style={{
+                    padding: 8,
+                    display: 'flex',
+                    flexDirection: 'row',
+                    flexWrap: 'wrap',
+                    columnGap: 16,
+                }}
+            >
+                {analysisColumnSections}
+            </div>
         </Section>
     );
 };
