d469c50897a18b78500b63fc7bfab2cfea673716
henrypgill <hpgill312@gmail.com>
2024-01-16 18:03:56 +0000

    updated to import aggregate card component

--- src/analysis/opviaAnalysis.tsx
+++ src/analysis/opviaAnalysis.tsx
@@ -1,26 +1,14 @@
+import { Card, Section } from '@blueprintjs/core';
+import { useAppSelector } from '../redux/store';
+import AggregateCard from './AggregateCard';
 import { OpviaTableColumn } from '../types/tableTypes';
 
-    return (
-        <Card
-            style={cardStyle}
-            onClick={() => setEditing(true)}
-            interactive={true}
-        >
-            <div>
-                <Icon icon={getAggregateIcon(aggregate.type)} size={20} />
-            </div>
-            <div>
-                <h2>
-                    {`${aggregate.type} ${column.columnName}`.toLowerCase()}
-                </h2>
-                <h3>{aggregate.value}</h3>
-            </div>
-        </Card>
-    );
+const AnalysisColumnSection: React.FC<OpviaTableColumn> = (column) => {
+    return <Card></Card>;
 };
 
 const Analysis = () => {
-    const analysis = useAppSelector((state) => state.analysis);
+    const { analysis } = useAppSelector((state) => state.table);
 
     if (!analysis.aggregates[0]) return <></>;
     const aggregateCards = analysis.aggregates.map((agg) => (
@@ -28,8 +16,16 @@ const Analysis = () => {
     ));
 
     return (
-        <Section title={'Aggregates'}>
-            <div className="flex-row">{aggregateCards}</div>
+        <Section
+            title={'Analysis'}
+            style={{
+                display: 'flex',
+                flexDirection: 'row',
+                flexWrap: 'wrap',
+                columnGap: 16,
+            }}
+        >
+            {aggregateCards}
         </Section>
     );
 };
